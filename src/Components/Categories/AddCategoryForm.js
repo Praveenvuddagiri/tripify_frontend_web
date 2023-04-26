1c0388e3af73ccf71962628c5cf47cfb4a0300c8
S.Naresh <64878148+sakthi406@users.noreply.github.com>
2023-04-26 09:52:11 +0000

    Api connect 1

--- src/Components/Categories/AddCategoryForm.js
+++ src/Components/Categories/AddCategoryForm.js
@@ -1,104 +1,122 @@
-import React from 'react';
 import { Box, Button, Grid, TextField, Typography } from '@mui/material';
-// import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
+import { addCategoryAPI } from '../../GlobalConstants';
+
 
 const AddCategoryForm = () => {
-  const handleSubmit = (e) =>{
-    e.preventDefault();
-      // api fetch for addCategory goes here
-      // setIsLoading(true);
-      // setError(null);
+const [categoryName, setCategoryName] = useState('');
+const [isLoading, setIsLoading] = useState(false);
+const [error, setError] = useState(null);
 
-      // try {
-      //     const response = await axios.post(`${baseAPI}${addCategoryAPI}`, {
-      //         CategoryName,
-      //         CategoryDescription,
-      //     }, {
-      //         headers: {
-      //             'Content-Type': 'application/json',
-      //             "Access-Control-Allow-Origin": "*",
-      //             "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
-      //         }
-      //     });
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  // api fetch for login goes here
+  setIsLoading(true);
+  setError(null);
 
-      //     const token = response.data.token;
-      //     localStorage.setItem("token", token);
-          
+  try {
+      const response = await axios.post(`${baseAPI}${addCategoryAPI}`, {
+          categoryName,
+          CategoryDescription,
+          CategoryImage,
+      }, {
+          headers: {
+              'Content-Type': 'application/json',
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+          }
+      });
+      
 
-      // } catch (error) {
+      const userData = response.data.user;
+      localStorage.setItem("user", JSON.stringify(userData));
+
+      if(userData){
+          dispatch({
+              type: "SET_USER",
+              user: userData
+          })
+          navigate('/');
+      }
+      
+
+  } catch (error) {
 
 
-      //     if (Math.floor(error.response.status / 100) === 5) {
-      //         error.response.data.color = 'rgb(255 99 0)';
-      //     } else {
-      //         error.response.data.color = 'red';
-      //     }
+      if (Math.floor(error.response.status / 100) === 5) {
+          error.response.data.color = 'rgb(255 99 0)';
+      } else {
+          error.response.data.color = 'red';
+      }
 
-      //     setError(error.response.data);
+      setError(error.response.data);
 
 
-      // } finally {
-      //     setIsLoading(false);
-      // }
+  } finally {
+      setIsLoading(false);
+  }
+};
   };
 
   return (
-    <Box m={3}>
+      <Box m={3}>
       <Typography variant="h4" align="center" gutterBottom>
-        Add Category
-      </Typography>
-      <Box component="form" onSubmit={handleSubmit}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              label="Name"
-              variant="outlined"
-              required
-              name="CategoryName"
-              // add any additional props you need for this input
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              label="Description"
-              variant="outlined"
-              required
-              name="CategoryDescription"
-              multiline
+      Add Category
+    </Typography>
+    <Box component="form" onSubmit={handleSubmit}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            label="Name"
+            variant="outlined"
+            required
+            name="CategoryName"
+            // add any additional props you need for this input
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            label="Description"
+            variant="outlined"
+            required
+            name="CategoryDescription"
+            multiline
+   
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <Button
+            variant="contained"
+            component="label"
+            fullWidth
+          >
+            Upload Images
+            <input
+              type="file"
+              multiple
+              name="CategoryImage"
+              hidden
      
             />
-          </Grid>
-          <Grid item xs={6}>
-            <Button
-              variant="contained"
-              component="label"
-              fullWidth
-            >
-              Upload Images
-              <input
-                type="file"
-                multiple
-                hidden
-       
-              />
-            </Button>
-          </Grid>
-          <Grid item xs={6}>
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              fullWidth
-            >
-              Add Category
-            </Button>
-          </Grid>
+          </Button>
         </Grid>
+        <Grid item xs={6}>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            fullWidth
+          >
+            Add Category
+          </Button>
+         </Grid>
+          </Grid>
+       </Box>
       </Box>
-    </Box>
-  );
-};
+    );
+  };
 
 export default AddCategoryForm;
--- src/GlobalConstants.js
+++ src/GlobalConstants.js
@@ -1,9 +1,8 @@
 export const AppName = "Tripify";
-export const baseAPI = "https://precious-jersey-wasp.cyclic.app/"
+export const baseAPI = "https://tripify-backend-0hxm.onrender.com/"
 export const loginAPI = "api/v1/admin/login";
 export const signUpAPI = "api/v1/signup";
 export const forgotPasswordAPI = "api/v1/forgotpassword";
 export const OtpSubmitAPI = "api/v1/verify/otp";
 export const OtpRegenerateAPI = "api/v1/regenerate/otp";
 export const addCategoryAPI = "api/v1/category/add";
-
